ed09dc4d05d239efec4755670a43de596732b49a
zhangpengzhi <geekzhang@163.com>
2019-01-14 22:20:29 +0800

    first commit

---
+++ server2/src/main/java/com/geekzhang/server2/clients/Clients.java
@@ -0,0 +1,17 @@
+package com.geekzhang.server2.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description:
+ * @author: zhangpengzhi<geekzhang @ 1 6 3 . com>
+ * @date: 2019/1/14 9:42 PM
+ * @version: V1.0
+ */
+@FeignClient("server1")
+public interface Clients {
+
+    @RequestMapping("/test")
+    String test();
+}
---
+++ server2/src/main/java/com/geekzhang/server2/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.geekzhang.server2.controller;
+
+import com.geekzhang.server2.clients.Clients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: zhangpengzhi<geekzhang @ 1 6 3 . com>
+ * @date: 2019/1/14 9:42 PM
+ * @version: V1.0
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private Clients clients;
+
+    @RequestMapping("/test")
+    public String test() {
+        return "This is server2";
+    }
+
+    @RequestMapping("/client")
+    public String client() {
+        return clients.test();
+    }
+}
